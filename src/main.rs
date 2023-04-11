8316141a3bd56236f068e3bbba41f524961eea26
xlzhang <wszxl516@163.com>
2023-04-11 15:54:52 +0800

    replace rust stack string.

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use std::ffi::CString;
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::time::Duration;
+
+struct ReplaceStack {
+    stack: (usize, usize),
+}
+
+#[derive(Debug)]
+struct StrBlock(usize, usize);
+impl StrBlock{
+    fn new(start: usize, len: usize)-> Self{
+        Self{0: start, 1: len}
+    }
+}
+
+
+impl ReplaceStack {
+    fn new() -> Result<Self, ()> {
+        match Self::find_stack() {
+            Ok(stack) => Ok(Self { stack }),
+            Err(_) => Err(()),
+        }
+    }
+    fn find_stack() -> Result<(usize, usize), ()> {
+        let map_file = File::open("/proc/self/maps").unwrap();
+        let stack = io::BufReader::new(map_file)
+            .lines()
+            .map(|line| line.unwrap_or("".to_string()))
+            .filter(|line| line.contains("[stack]"))
+            .collect::<Vec<String>>();
+        if stack.is_empty() {
+            return Err(());
+        }
+        let stack_range: [&str; 2] = stack[0].split(" ").collect::<Vec<&str>>()[0]
+            .splitn(2, "-")
+            .collect::<Vec<&str>>()
+            .try_into()
+            .unwrap();
+        let start = usize::from_str_radix(stack_range[0], 16).unwrap_or(0);
+        let end = usize::from_str_radix(stack_range[1], 16).unwrap_or(0);
+        if start != 0 && end != 0 {
+            return Ok((start, end));
+        }
+        Err(())
+    }
+    fn find_string_addr(&self, name: &String) -> Result<Vec<StrBlock>, ()> {
+        let mut tmp = Vec::<u8>::new();
+        let mut argv_addr = Vec::<StrBlock>::new();
+        let (start, end) = self.stack;
+        for i in start..end {
+            tmp.clear();
+            for x in 0..name.len() {
+                if i + x >= end {
+                    break;
+                }
+                tmp.push(unsafe { std::ptr::read((i + x) as *const u8) })
+            }
+            let p_name = unsafe { CString::from_vec_unchecked(tmp.clone()) }
+                .to_str()
+                .unwrap_or("")
+                .to_string();
+            if p_name.contains(name) {
+                let mut len = 0usize;
+                loop {
+                    if (unsafe { std::ptr::read((i + len) as *const u8) }).eq(&0){
+                        break
+                    }
+                    len += 1;
+                }
+                argv_addr.push(StrBlock::new(i, len))
+            }
+        }
+        if argv_addr.is_empty() {
+            return Err(());
+        }
+        Ok(argv_addr)
+    }
+    fn replace_string(block: StrBlock, name: &str) {
+        let name_len = name.len();
+        let str_ptr: &mut [u8] =
+            unsafe { std::slice::from_raw_parts_mut(block.0 as *mut u8, block.1) };
+        str_ptr.fill(0);
+        for c in 0..name_len {
+            str_ptr[c] = name.as_bytes()[c];
+        }
+    }
+}
+
+fn main() {
+    let args = std::env::args();
+    let args_str = args.collect::<Vec<String>>();
+    let st = ReplaceStack::new().unwrap();
+    let argv_addr = st.find_string_addr(&args_str[0]).unwrap();
+    println!("argv: {:?}", argv_addr);
+    for addr in argv_addr {
+        ReplaceStack::replace_string(addr, "[test_name]");
+    }
+    loop {
+        let args = std::env::args();
+        let args_str = args.collect::<Vec<String>>();
+        println!("{}", args_str[0]);
+        std::thread::sleep(Duration::from_secs(1))
+    }
+}
